785fe2154d267bc941f0b02c84e10c156675d4ea
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-01-08 23:57:55 +0100

    vite

--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -1,11 +1,11 @@
 
 <script>
-    import {siGithub,siInstagram,siYoutube,siLinkedin,siMastodon} from 'simple-icons';
+    // import {siGithub,siInstagram,siYoutube,siLinkedin,siMastodon} from 'simple-icons';
 	import SocialComponent from './Social_Component.svelte';
 </script>
 
 <div class="m-auto block w-max">
-    <div class="inline-block m-8 mb-0 hover:scale-[105%] transition-transform">
+    <!-- <div class="inline-block m-8 mb-0 hover:scale-[105%] transition-transform">
         <SocialComponent redirect_uri="https://instagram.com/lanoe_35" icon={siInstagram}></SocialComponent>
     </div>
     <div class="inline-block m-8 mb-0 hover:scale-[105%] transition-transform">
@@ -19,5 +19,5 @@
     </div>
     <div class="inline-block m-8 mb-0 hover:scale-[105%] transition-transform">
         <SocialComponent redirect_uri="https://github.com/aquabx" icon={siGithub}></SocialComponent>
-    </div>
+    </div> -->
 </div>
